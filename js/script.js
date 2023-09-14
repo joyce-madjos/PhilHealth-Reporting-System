5f7540269abcf9cde9cd1170922a86741b546c6f
joyce-madjos <madjos.joyce.arapan@gmail.com>
2023-09-14 19:03:19 +0800

    fix heading text per change tab

---
+++ js/script.js
@@ -0,0 +1,15 @@
+document.getElementById("member").addEventListener("click", function () {
+  document.getElementById("heading").innerText = "Member Information";
+});
+
+document.getElementById("employer").addEventListener("click", function () {
+  document.getElementById("heading").innerText = "Employer Information";
+});
+
+document.getElementById("remittance").addEventListener("click", function () {
+  document.getElementById("heading").innerText = "Remittance";
+});
+
+document.getElementById("receipt").addEventListener("click", function () {
+  document.getElementById("heading").innerText = "Receipt";
+});
